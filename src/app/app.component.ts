ad005ae991ec3211d6c51e5a398a84ede7e6cf9d
rafa0206 <rafaelfernandes0206@gmail.com>
2021-04-12 20:53:24 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto-databind';
+
+  numero: number;
+  escolher(){
+    this.numero=Math.floor(Math.random()*100)+1;
+  }
+
 }
